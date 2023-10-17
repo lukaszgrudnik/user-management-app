eaa9f56d52f9cd7d9134a79a74c71a6c8dab0ce9
lukaszgrudnik <lukasz.grudnik@outlook.com>
2023-10-17 16:28:59 +0200

    unify styles for creator and editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { UserModule } from './users/user.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SpinnerComponent } from './common/spinner/spinner.component';
 
 @NgModule({
-  declarations: [AppComponent, NotFoundComponent],
+  declarations: [AppComponent, NotFoundComponent, SpinnerComponent],
   imports: [BrowserModule, AppRoutingModule, UserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
