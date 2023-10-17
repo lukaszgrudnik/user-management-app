9c6cbcacff2c1e68331219c42295e82b1c142f28
lukaszgrudnik <lukasz.grudnik@outlook.com>
2023-10-17 16:30:51 +0200

    fix lost core interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './users/user.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SpinnerComponent } from './common/spinner/spinner.component';
+import { HttpCoreInterceptor } from './core/http-core.interceptor';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent, SpinnerComponent],
   imports: [BrowserModule, AppRoutingModule, UserModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpCoreInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
